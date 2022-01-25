11c8141abc8e2a9e18e107c2831f83b546a68862
Linh <90367935+lq-hoang@users.noreply.github.com>
2022-01-25 01:52:52 -0500

    added variable types to User method parameters

--- src/main/entity/Hamster.py
+++ src/main/entity/Hamster.py
@@ -4,7 +4,7 @@ for hamsters under a user's ownership.
 
 
 class Hamster:
-    def __init__(self, name, breed, sex, age, weight=-1, notes=""):
+    def __init__(self, name: str, breed: str, sex: str, age: [int], weight: float, notes: str):
         self.name = name
         self.breed = breed
         self.sex = sex
@@ -12,40 +12,41 @@ class Hamster:
         self.weight = weight
         self.notes = notes
 
-    def display_profile(self):
-        return ("Name:", self.name, "\nBreed:", self.breed, "\nSex:", self.sex,
-                "\nAge:", self.age, "\nWeight (g):", self.weight,
-                "\nNotes:", self.notes)
+    def display_profile(self) -> str:
+        profile = "Name:", self.name, "\nBreed:", self.breed, "\nSex:", self.sex, \
+                  "\nAge:", self.age, "\nWeight (g):", self.weight, \
+                  "\nNotes:", self.notes
+        return profile
 
-    def set_name(self, new_name):
+    def set_name(self, new_name: str) -> None:
         self.name = new_name
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.name
 
-    def set_breed(self, new_breed):
+    def set_breed(self, new_breed: str) -> None:
         self.breed = new_breed
 
-    def get_breed(self):
+    def get_breed(self) -> str:
         return self.breed
 
-    def set_sex(self, new_sex):
+    def set_sex(self, new_sex: str) -> None:
         self.sex = new_sex
 
-    def get_sex(self):
+    def get_sex(self) -> str:
         return self.sex
 
-    def set_age(self, new_age):
+    def set_age(self, new_age: str) -> None:
         self.age = new_age
 
-    def get_age(self):
+    def get_age(self) -> [int]:
         return self.age
 
-    def set_weight(self, new_weight):
+    def set_weight(self, new_weight: float) -> None:
         self.weight = new_weight
 
-    def get_weight(self):
+    def get_weight(self) -> float:
         return self.weight
 
-    def set_notes(self, new_note):
+    def set_notes(self, new_note: str) -> None:
         self.notes = new_note
